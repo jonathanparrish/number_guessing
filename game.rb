60e8e5516f7e0fb687c5d2527734087596eeec3f
Jonathan Parrish <jonathan.s.parrish@gmail.com>
2015-01-13 16:01:28 -0500

    Final Game

---
+++ game.rb
@@ -0,0 +1,53 @@
+def max_number
+  return 100
+end
+
+def greeting
+  puts "Guess the random number."
+  puts "It's between 1 and #{max_number}."
+  puts "You get 5 tries"
+  puts "Go!"
+end
+
+def get_number_from_user
+  gets.chomp.to_i
+end
+
+def remaining_guesses(count)
+  if 5-count == 1
+    puts "You have 1 guess remaining."
+  else
+    puts "You have #{5-count} guesses remaining."
+  end
+end
+
+guess_array=[]
+comp_number = rand (1 .. max_number)
+still_playing = true
+puts comp_number
+greeting
+count = 1
+guess = get_number_from_user
+while count < 5 && still_playing
+  remaining_guesses(count)
+  if guess == comp_number
+    puts "But you got it in #{count + 1}, Congratulations!"
+    still_playing = false
+    exit
+  elsif
+    guess_array.include?(guess)
+    puts "And you already guessed that one smart guy!!"
+  elsif guess > comp_number
+    guess_array << guess
+    puts "Try again the number is lower!"
+  else
+    guess_array << guess
+    puts "Try again the number is higher!"
+  end
+  count = count + 1
+  guess = get_number_from_user
+end
+if still_playing == true
+  puts "You're out of moves!"
+end
+puts guess_array
